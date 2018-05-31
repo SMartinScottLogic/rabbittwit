a6ae8c90a7f25c9ddb07384139203f9fc79e54dd
Simon Martin <havvoric@gmail.com>
2018-05-31 21:28:39 +0100

    cURL download from rabbit MQ.

--- receive.php
+++ receive.php
@@ -1,4 +1,5 @@
 <?php
+$out_dir = "/";
 
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -12,7 +13,23 @@ $channel->queue_declare('hello', false, true, false, false);
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg) {
-  echo " [x] Received ", $msg->body, "\n";
+  global $out_dir;
+
+  $body = json_decode($msg->body, TRUE);
+  $url = $body['url'];
+  $name = basename($url);
+  echo " [x] Received ${body['url']} -> ${name}\n";
+
+  $fp = fopen("${out_dir}/${name}", 'w+');
+
+  $ch = curl_init(str_replace(" ", "%20", $url));
+  curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+  curl_setopt($ch, CURLOPT_FILE, $fp);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+  curl_exec($ch);
+  curl_close($ch);
+  fclose($fp);
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
