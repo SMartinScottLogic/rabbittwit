7e4837202b7651fd7cb1dbd4c735d1631c370e62
Simon Martin <havvoric@gmail.com>
2018-07-10 09:18:38 +0100

    Add example user feed stream.

--- twitter.php
+++ twitter.php
@@ -76,6 +76,157 @@ function upsert_image($url, $user) {
   );
 }
  */
+function some_callback($tweet)
+{
+  // gets called for every new streamed message
+  // gets called with $message = NULL once per second
+
+  if ($tweet !== null) {
+    $images = array();
+    print_r($tweet);
+    if(isset($tweet->id)) {
+      $id = $tweet->id;
+      // print "ID: " . $id . "\n";
+
+      if(!isset($highest_id)) {
+        $highest_id = $id;
+      } else {
+        $highest_id = max($highest_id, $id);
+      }
+      if(!isset($lowest_id)) {
+        $lowest_id = $id;
+      } else {
+        $lowest_id = min($lowest_id, $id);
+      }
+    }
+
+    //print $tweet->text . "\n";
+    $num_media = 0;
+    $num_extended_media=0;
+    $num_extended_media_videos = 0;
+    $num_retweeted_media=0;
+    $num_retweeted_extended_media=0;
+    if(isset($tweet->entities) ) {
+      if( isset($tweet->entities->media)) {
+        $num_media = count($tweet->entities->media);
+        foreach($tweet->entities->media as $media) {
+          //print_r($media);
+          unset($url);
+          if(isset($media->media_url)) {
+            $url = $media->media_url;
+          } else if(isset($media->media_url_https)) {
+            $url = $media->media_url_https;
+          }
+	  if(isset($url)) {
+            $large_url = $url.':large';
+	    $images[$large_url] = array('url' => $large_url, 'user' => $tweet->user->id_str);
+            //upsert_image($url.":large", $tweet->user->id_str);
+          }
+        }
+      }
+    }
+    if(isset($tweet->extended_entities) ) {
+      if( isset($tweet->extended_entities->media)) {
+        $num_extended_media = count($tweet->extended_entities->media);
+        foreach($tweet->extended_entities->media as $media) {
+          //print_r($media);
+          unset($url);
+          if(isset($media->media_url)) {
+            $url = $media->media_url;
+          } else if(isset($media->media_url_https)) {
+            $url = $media->media_url_https;
+          }
+          if(isset($url)) {
+            $large_url = $url.':large';
+	    $images[$large_url] = array('url' => $large_url, 'user' => $tweet->user->id_str);
+            //upsert_image($url.":large", $tweet->user->id_str);
+          }
+        }
+      }
+    }
+    
+    if(isset($tweet->extended_entities) ) {
+      if( isset($tweet->extended_entities->media)) {
+        $num_extended_media_videos = 0;
+        foreach($tweet->extended_entities->media as $media) {
+          //print_r($media);
+          if(isset($media->video_info) && isset($media->video_info->variants)) {
+            foreach($media->video_info->variants as $variant) {
+              unset($url);
+              if(isset($variant->url)) {
+                $url = $variant->url;
+              }
+              if(isset($url)) {
+                $num_extended_media_videos ++;
+                $large_url = $url;
+	        $images[$large_url] = array('url' => $large_url, 'user' => $tweet->user->id_str);
+                //upsert_image($url, $tweet->user->id_str);
+              }
+            }
+          }
+        }
+      }
+    }
+    if(isset($tweet->retweeted_status) && isset($tweet->retweeted_status->entities) && isset($tweet->retweeted_status->entities->media)) {
+      $num_retweeted_media = count($tweet->retweeted_status->entities->media);
+      foreach($tweet->retweeted_status->entities->media as $media) {
+        //print_r($media);
+        unset($url);
+        if(isset($media->media_url)) {
+          $url = $media->media_url;
+        } else if(isset($media->media_url_https)) {
+          $url = $media->media_url_https;
+        }
+        if(isset($url)) {
+          $large_url = $url . ':large';
+	  $images[$large_url] = array('url' => $large_url, 'user' => $tweet->retweeted_status->user->id_str);
+          //upsert_image($url.":large", $tweet->retweeted_status->user->id_str);
+        }
+      }
+    }
+    if(isset($tweet->retweeted_status) && isset($tweet->retweeted_status->extended_entities) && isset($tweet->retweeted_status->extended_entities->media)) {
+      $num_retweeted_media = count($tweet->retweeted_status->extended_entities->media);
+      foreach($tweet->retweeted_status->extended_entities->media as $media) {
+        //print_r($media);
+        unset($url);
+        if(isset($media->media_url)) {
+          $url = $media->media_url;
+        } else if(isset($media->media_url_https)) {
+          $url = $media->media_url_https;
+        }
+        if(isset($url)) {
+          $large_url = $url . ':large';
+	  $images[$large_url] = array('url' => $large_url, 'user' => $tweet->retweeted_status->user->id_str);
+          //upsert_image($url.":large", $tweet->retweeted_status->user->id_str);
+        }
+      }
+    }
+    if( $num_media+$num_extended_media+$num_retweeted_media+$num_retweeted_extended_media+$num_extended_media_videos>0) {
+      fprintf(STDERR, "%s\t%s\t%s\t%s\t%s\t%s\n", $id, $num_media, $num_extended_media, $num_retweeted_media, $num_retweeted_extended_media, $num_extended_media_videos);
+    }
+
+    foreach($images as $image) {
+      upsert_image($image);
+    }
+    flush();
+  }
+
+  // return false to continue streaming
+  // return true to close the stream
+
+  // close streaming after 1 minute for this simple sample
+  // don't rely on globals in your code!
+  if (time() - $GLOBALS['time_start'] >= 60) {
+    return true;
+  }
+
+  return false;
+}
+
+// set the streaming callback in Codebird
+$cb->setStreamingCallback('some_callback');
+
+
 function upsert_image($image) {
   global $channel;
 
@@ -265,6 +416,13 @@ for($page=0; $page<$max_pages; $page++) {
     break;
   }
 }
+// for canceling, see callback function body
+// not considered good practice in real world!
+$GLOBALS['time_start'] = time();
+
+// Second, start consuming the stream:
+$reply = $cb->user();
+
 $fp = fopen(dirname(__FILE__) . '/highest_id.txt', "w");
 if($fp) {
   fprintf($fp, "%s\n", $highest_id);
